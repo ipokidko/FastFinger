f786be6a8335feafce458f27328cae82b5ec2764
kostya <3383389@gmail.com>
2016-09-13 22:07:09 +0300

    Make counter = 0 after pressing Start button and display score after clicking buttons close #5, close #3

--- app/src/main/java/com/a256devs/fastfinger/MainActivity.java
+++ app/src/main/java/com/a256devs/fastfinger/MainActivity.java
@@ -3,6 +3,7 @@ package com.a256devs.fastfinger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void increaseCounter(View view) {
         counter++;
+        displayCounter(counter);
+    }
+
+    public void startButton(View viev) {
+        counter = 0;
+        displayCounter(counter);
+    }
+
+    public void displayCounter(int counter) {
+        TextView count = (TextView) findViewById(R.id.score);
+        count.setText(String.valueOf(counter));
     }
 
 }
