4f9bcf8c2bf6e0dd76c45eb5fb9bb54d11d06f89
kostya <3383389@gmail.com>
2016-09-13 21:40:53 +0300

    Increase counter after any click close #4

--- app/src/main/java/com/a256devs/fastfinger/MainActivity.java
+++ app/src/main/java/com/a256devs/fastfinger/MainActivity.java
@@ -2,12 +2,20 @@ package com.a256devs.fastfinger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increaseCounter(View view) {
+        counter++;
+    }
+
 }
