e4b3abccc71104e29ffd0b68363b0c633567e44d
ipokidko <igor.pokidko@gmail.com>
2016-09-13 22:33:42 +0300

    code refactoring

--- app/src/main/java/com/a256devs/fastfinger/MainActivity.java
+++ app/src/main/java/com/a256devs/fastfinger/MainActivity.java
@@ -1,19 +1,24 @@
 package com.a256devs.fastfinger;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int counter = 0;
+    private TextView mScoreInfoTexyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mScoreInfoTexyView = (TextView) findViewById(R.id.score);
     }
+
 
     public void increaseCounter(View view) {
         counter++;
@@ -26,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayCounter(int counter) {
-        TextView count = (TextView) findViewById(R.id.score);
-        count.setText(String.valueOf(counter));
+        mScoreInfoTexyView.setText(String.valueOf(counter));
     }
 
 }
